851cb1a02709ff97873d57540d5abc9c54f8b178
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-07-25 16:02:19 +0800

    Create Story struct Replace constants with Story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let story = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
     
     @IBAction func choiceMade(_ sender: UIButton) {
     }
@@ -24,9 +22,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
     }
 
 }
